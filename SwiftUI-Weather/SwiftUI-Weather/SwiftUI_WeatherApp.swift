85037f40f0560a5907fe53dd9b6afca6501cc170
redbuilderOne <forsaken2492@yandex.ru>
2022-04-05 11:06:04 +0300

    first commit

---
+++ SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Дмитрий Скворцов on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
